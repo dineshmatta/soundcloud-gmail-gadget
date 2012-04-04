4cecbb99fb7250bbe4ab193f379f9c6f91ae390d
Tobias Bielohlawek <tobi@soundcloud.com>
2012-04-04 16:35:56 +0200

    update gmail gadget to use jsonp

--- google-app/main.js
+++ google-app/main.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
   $(document.body).gadgetize({
+    dataType: 'jsonp',
     urls: google.contentmatch.getContentMatches(),
     updateHeightCallback: function(height) {
       gadgets.window.adjustHeight(height);
